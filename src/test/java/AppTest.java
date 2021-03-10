edc9097784d7ba6df55914524c1570fce4f0b767
AliakseiBelabrovik <71708619+AliakseiBelabrovik@users.noreply.github.com>
2021-03-10 20:01:37 +0100

    Added tests for specific characters. BeforeAll method generates a random specific character that is allowed.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,26 @@
 import org.junit.jupiter.api.*;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
+    static char specificCharacter;
+
     @BeforeAll
     public static void init() {
         System.out.println("Testing the application");
+
+        Random random = new Random();
+        while (true) {
+            int valueOfChar = random.nextInt(64 - 33 + 1) + 33;
+            if (valueOfChar == 33 || (valueOfChar >= 35 && valueOfChar <= 37) || valueOfChar == 40 || valueOfChar == 41
+                    || valueOfChar == 47 || valueOfChar == 63 || valueOfChar == 64) {
+                specificCharacter = (char) valueOfChar;
+            }
+        }
+        // Special characters a. ASCII value (- 40 ) - 41 # - 35 $ -36 ? -63 ! - 33 % - 37 / -47 @ - 64
     }
 
 
@@ -17,8 +31,8 @@ public class AppTest {
 
 
 
-    //Kennwort muss zwischen 8 und 25 Zeichen
-    //muss mindestens 1 klein- und 1 Großbuchstaben haben
+    //Kennwort muss zwischen 8 und 25 Zeichen - ok
+    //muss mindestens 1 klein- und 1 Großbuchstaben haben - ok
     //muss Zahlen enthalten
     //muss eines der Sonderzeichen enthalten
 
@@ -27,28 +41,54 @@ public class AppTest {
     @DisplayName("Testing the maximum length of 25 characters")
     void testForMaximumLength() {
         assertFalse(App.checkPassword("Password1@TestingMustBeAtLeast25"), "The length of the password " +
-                "must be maximum of 25 characters."); //test must return false, because input
-        //is 32 characters long
+                "must be maximum of 25 characters. The method must return false."); //method must return false,
+        // because input is 32 characters long
     }
 
-    @Test //test must return false, because input is 7 characters long
+    @Test //method must return false, because input is 7 characters long
     @DisplayName("Testing the minimum length of 8 characters")
     void testForMinimumLength() {
         assertFalse(App.checkPassword("Passw1@"), "The length of the password must be minimum of " +
-                "8 characters.");
+                "8 characters. The method must return false.");
     }
 
     @Test //must return false, since there are not lowercase letters
     @DisplayName("Testing the password contains both small and big letters")
     void testForSmallLetters() {
-        assertFalse(App.checkPassword("PASSWORD111!"), "The password must contain at least one lowercase letter");
+        assertFalse(App.checkPassword("PASSWORD111!"), "The password must contain at least one lowercase " +
+                "letter. The method must return false.");
     }
 
 
     @Test //must return false, since there are not capital letters
     @DisplayName("Testing the password contains both small and big letters")
     void testForBigLetters() {
-        assertFalse(App.checkPassword("password111@"), "The password must contain at least one capital letter");
+        assertFalse(App.checkPassword("password111@"), "The password must contain at least one capital " +
+                "letter. The method must return false.");
     }
+
+    @Test //the method must return false, since there is no number
+    @DisplayName("Testing the password contains at least one number")
+    void testForNumber() {
+        assertFalse(App.checkPassword("ThePassword%"), "The password must contain at least one number. " +
+                "The method must return false.");
+    }
+
+
+    @Test // Special characters a. ASCII value (- 40 ) - 41 # - 35 $ -36 ? -63 ! - 33 % - 37 / -47 @ - 64
+    @DisplayName("Testing for not allowed special characters")
+    void testForSpecialCharacters() {
+        assertFalse(App.checkPassword("Pasd111:" + specificCharacter), "The method must return false if " +
+                "not allowed specific characters are used.");
+    }
+    @Test
+    @DisplayName("Testing for special characters")
+    void testForSpecialCharacters_Scenario2() {
+        assertTrue(App.checkPassword("()$?!%/Pas!w@ord1#" + specificCharacter), "The method must allow the " +
+                "following specific characters: ()#$?!%/@");
+    }
+
+
+
 
 }
