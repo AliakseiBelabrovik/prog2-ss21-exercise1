9632d7ace4b17fc61396253675d884438aea33eb
AliakseiBelabrovik <71708619+AliakseiBelabrovik@users.noreply.github.com>
2021-03-10 22:59:28 +0100

    Implemented method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,34 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
 
     public static boolean checkPassword(String password) {
 
+        //?= Lookahead assertion
+        //?! Negative lookahead
+        /*
+        \\S - no whitespace characters
+        (?=.*[a-z]) - lowercase letters
+        (?=.*[A-Z]) - uppercase letters
+        (?=.*[0-9]) - numbers
+        (?=.*[()#$?!%/@]) - specific characters
+        .{8,25} - between 8 and 25
+         */
+        Pattern pattern = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[()#$?!%/@])[\\S&&[\\w[()#$?!%/@]]]{8,25}");
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
 
-        return true;
+
+
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please insert the password: ");
+        String password = scanner.nextLine();
+        System.out.println(password);
+        System.out.println(checkPassword(password));
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,6 +18,7 @@ public class AppTest {
             if (valueOfChar == 33 || (valueOfChar >= 35 && valueOfChar <= 37) || valueOfChar == 40 || valueOfChar == 41
                     || valueOfChar == 47 || valueOfChar == 63 || valueOfChar == 64) {
                 specificCharacter = (char) valueOfChar;
+                break;
             }
         }
         // Special characters a. ASCII value (- 40 ) - 41 # - 35 $ -36 ? -63 ! - 33 % - 37 / -47 @ - 64
@@ -36,9 +37,15 @@ public class AppTest {
     //muss Zahlen enthalten
     //muss eines der Sonderzeichen enthalten
 
+    @Test
+    @DisplayName("1: Test for correct password.")
+    void testForCorrectPassword() {
+        assertTrue(App.checkPassword("@Pasd111%!"), "The password @Pasd111%! is an allowed password. " +
+                "Check the method again.");
+    }
 
     @Test
-    @DisplayName("Testing the maximum length of 25 characters")
+    @DisplayName("2: Testing the maximum length of 25 characters")
     void testForMaximumLength() {
         assertFalse(App.checkPassword("Password1@TestingMustBeAtLeast25"), "The length of the password " +
                 "must be maximum of 25 characters. The method must return false."); //method must return false,
@@ -46,14 +53,14 @@ public class AppTest {
     }
 
     @Test //method must return false, because input is 7 characters long
-    @DisplayName("Testing the minimum length of 8 characters")
+    @DisplayName("3: Testing the minimum length of 8 characters")
     void testForMinimumLength() {
         assertFalse(App.checkPassword("Passw1@"), "The length of the password must be minimum of " +
                 "8 characters. The method must return false.");
     }
 
     @Test //must return false, since there are not lowercase letters
-    @DisplayName("Testing the password contains both small and big letters")
+    @DisplayName("4: Testing the password contains both small letters.")
     void testForSmallLetters() {
         assertFalse(App.checkPassword("PASSWORD111!"), "The password must contain at least one lowercase " +
                 "letter. The method must return false.");
@@ -61,14 +68,14 @@ public class AppTest {
 
 
     @Test //must return false, since there are not capital letters
-    @DisplayName("Testing the password contains both small and big letters")
+    @DisplayName("5: Testing the password contains both small and big letters")
     void testForBigLetters() {
         assertFalse(App.checkPassword("password111@"), "The password must contain at least one capital " +
                 "letter. The method must return false.");
     }
 
     @Test //the method must return false, since there is no number
-    @DisplayName("Testing the password contains at least one number")
+    @DisplayName("6: Testing the password contains at least one number")
     void testForNumber() {
         assertFalse(App.checkPassword("ThePassword%"), "The password must contain at least one number. " +
                 "The method must return false.");
@@ -76,19 +83,24 @@ public class AppTest {
 
 
     @Test // Special characters a. ASCII value (- 40 ) - 41 # - 35 $ -36 ? -63 ! - 33 % - 37 / -47 @ - 64
-    @DisplayName("Testing for not allowed special characters")
+    @DisplayName("7: Testing for not allowed special characters")
     void testForSpecialCharacters() {
         assertFalse(App.checkPassword("Pasd111:" + specificCharacter), "The method must return false if " +
-                "not allowed specific characters are used.");
+                "not allowed specific characters are used." + specificCharacter);
     }
     @Test
-    @DisplayName("Testing for special characters")
+    @DisplayName("8: Testing for special characters")
     void testForSpecialCharacters_Scenario2() {
         assertTrue(App.checkPassword("()$?!%/Pas!w@ord1#" + specificCharacter), "The method must allow the " +
                 "following specific characters: ()#$?!%/@");
     }
 
-
+    @Test
+    @DisplayName("9: Testing white spaces")
+    void testForWhiteSpaces() {
+        assertFalse(App.checkPassword("()$?!% /Pas!w@ord1#"), "The method must return false, because " +
+                "no whitespaces are allowed");
+    }
 
 
 }
